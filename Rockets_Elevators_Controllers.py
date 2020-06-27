4ff0b659f2f0d84a24a098e6d36b31ab9f517f32
said bourakkadi <saidbourakkadi@gmail.com>
2020-06-27 18:07:57 -0400

    Add the class

--- Rockets_Elevators_Controllers.py
+++ Rockets_Elevators_Controllers.py
@@ -1,362 +1,63 @@
-
-// variables
-// var firstListUp = 0;
-// var firstListDown = 0;
-
-
-
-//class constructor cages
-class Cage {
-    constructor(id, column, battery, levelActual, direction) {
-        this.battery = battery;
-        this.column = column;
-        this.id = id;
-
-        this.statusDoor = "closed"; // open or closed
-        this.position_door_x = 0; // x = 0 door closed, x=150 door open
-
-        this.timing_door = 3000; // max 3s for open or close
-        this.timing_floor = 20000; // max 20 s by floor
-        this.listUp = [];        // list order up and atopped step by step
-        this.listUpOrder = []; // order up this list
-        this.listDown = [];   // list order down and atopped step by step
-        this.listDownOrder = []; // order down this list
-        this.direction = direction; // up, down, idle
-        this.levelActual = levelActual; // level cage floor from 0 to 10
-        this.levelCall = [];
-        this.levelAsk = [];
-        this.sensor_status_cage = "Clear"; // security sensor obstruct door cage 
-        this.close_button = "inactif"; // button close door
-        this.open_button = "inactif"; // button open door
-
-        console.log("Cage : " + this.id + " levelActual : " + this.levelActual);
-    }
-    open_door() {
-
-        for (var x = 0; x < 3; x++) {
-            this.position_door_x = this.position_door_x + 50;
-            this.statusDoor = "open";
-            console.log("being closed 3 second : " + this.position_door_x + " status door : " + this.statusDoor);
-            this.Timer(1000);
-        } this.close_door();
-
-
-
-    }
-
-    close_door() {
-
-        for (var x = 0; x < 3; x++) {
-            this.position_door_x = this.position_door_x - 50;
-            this.statusDoor = "closed";
-            console.log("being open 3 second : " + this.position_door_x + " status door : " + this.statusDoor);
-            this.Timer(1000);
-        }
-
-
-    }
-    Timer(milliseconds) {
-        const date = Date.now();
-        let currentDate = null;
-        do {
-            currentDate = Date.now();
-        } while (currentDate - date < milliseconds);
-    }
-    Call_level(call) {
-        if (this.levelActual === call) {
-            console.log("You are in this floor")
-            this.open_door();
-        }
-        else if (this.levelActual < call) {
-            this.listUp.push(call)
-            this.order_area_ascendant(this.listUp);
-            console.log("we up " + " listUp : " + this.listUp);
-            this.move();
-        } else {
-            this.listDown.push(call)
-            this.order_area_descendant(this.listDown);
-            console.log("we down" + " listDown : " + this.listDown);
-            this.move();
-        }
-    }
-    order_area_ascendant(list) {
-        list.sort(function (a, b) { return a - b });
-        console.log("after tri listUP: " + this.listUp);
-    }
-
-    order_area_descendant(list) {
-        list.sort(function (a, b) { return b - a });
-        console.log("after tri listDown: " + this.listDown);
-    }
-    move() {
-        if (this.direction === "idle" && this.listUp != null) {
-            this.moveUp(this.listUp);
-        } else if (this.direction === "idle" && this.lisDown != null) {
-            this.moveDown();
-        } else if (this.direction === "up" && this.listUp != null) {
-            if (this.levelActual === this.listUp[0]) {
-                this.open_door();
-
-            } else {
-                this.moveUp(this.listUp[0]);
-            }
-
-        } else if (this.direction === "down" && this.listDown != null) {
-            if (this.levelActual === this.listUp[0]) {
-                this.open_door();
-            } else {
-                this.moveDown(this.listDown[0]);
-            }
-        } else { this.direction = "idle"; }
-    } moveUp(e) {
-        if (this.listUp[0] != null && (this.levelActual < this.listUp[0])) {
-            this.Timer(3000);
-
-            this.levelActual = this.levelActual + 1;
-            console.log("levelActual is: " + this.levelActual);
-            if (this.levelActual === e) {
-                this.open_door();
-                this.listUp.splice(0, 1);
-                console.log("splice : " + this.listUp);
-                this.move();
-
-            } else {
-                this.move();
-
-            }
-        } else {
-            this.direction = "down";
-            this.move();
-
-        } console.log("level actual is :" + this.levelActual);
-    }
-    moveDown(e) {
-        if (this.listDown[0] != null) {
-            this.Timer(3000);
-            this.levelActual = this.levelActual - 1;
-            console.log("levelActual is: " + this.levelActual);
-            if (this.levelActual === e) {
-                this.open_door();
-                this.listDown.splice(0, 1);
-                console.log("splice : " + this.listDown);
-            }
-            else if (this.listDown != null || this.listDown != null) {
-                this.move();
-            }
-
-
-        } else if (this.listUp[0] != null) {
-            this.direction = "up";
-            this.move();
-
-        }
-        else {
-            this.direction = "idle";
-            console.log("level actual is :" + this.levelActual);
-        }
-
-    }
-
-    // this compute counter step supposed the elevators 
-    //started from floor number 0 until floor number 10
-    // on button floor pressed the fonction counterStep for cage1 and cage 2
-
-    counterStep(levelAsk, directionAsk) {
-        console.log("the compute start");
-        var diff = this.levelActual - levelAsk;
-        var sum = this.levelActual + levelAsk;
-
-        var counterStepCage = 0;
-        if (this.direction === "idle") {
-            counterStepCage = Math.abs(this.levelActual - levelAsk);
-            console.log("levelAsk : " + levelAsk + " direction : " + directionAsk + " counterstep : " + counterStepCage);
-
-
-            return counterStepCage;
-        }
-
-        else if (directionAsk === "up" && this.direction === "up") {
-
-            if (levelAsk >= this.levelActual) {
-                counterStepCage = diff * (-1);
-                console.log("levelAsk : " + levelAsk + " direction : " + directionAsk + " counterstep : " + counterStepCage);
-
-                return counterStepCage; //, levelAsk, directionAsk;
-
-            } else {
-                counterStepCage = (20 - diff);
-                console.log("levelAsk : " + levelAsk + " direction : " + directionAsk + " counterstep : " + counterStepCage);
-
-
-                return counterStepCage;//, levelAsk, directionAsk;
-            }
-
-
-        } else if (directionAsk == "up" && this.direction == "down") {
-            counterStepCage = sum;
-            console.log("levelAsk : " + levelAsk + " direction : " + directionAsk + " counterstep : " + counterStepCage);
-
-            return counterStepCage, levelAsk, directionAsk;
-        } else if (directionAsk == "down" && this.direction == "up") {
-            counterStepCage = (20 - sum);
-
-            console.log("levelAsk : " + levelAsk + " direction : " + directionAsk + " counterstep : " + counterStepCage);
-
-            return counterStepCage;//, levelAsk, directionAsk;
-        } else if (directionAsk == "down" && this.direction == "down") {
-            if (levelAsk <= this.levelActual) {
-                counterStepCage = diff;
-                console.log("levelAsk : " + levelAsk + " direction : " + directionAsk + " counterstep : " + counterStepCage);
-
-                return counterStepCage;//, levelAsk, directionAsk;
-
-            } else {
-                counterStepCage = sum;
-                console.log("levelAsk : " + levelAsk + " direction : " + directionAsk + " counterstep : " + counterStepCage);
-
-                return counterStepCage;//, levelAsk, directionAsk;
-            }
-        }
-
-
-    }
-
-}
-// choice the best cage at asking
-
-battery_compute_BestTimingCage = function BestTimingCage(cage_1, cage_2) {
-    if (cage_1 <= cage_2) {
-        console.log("the Controller choice : cage1");
-        return "cage1";
-
-    } else {
-        console.log("the Controller choice : cage2");
-        return "cage2";
-    }
-};
-
-
-
-
-//////////////////////////////////////////////////////////////////
-// compute function calculeted shorter steps for each elevator, //
-//and send order(levelAsk, direction) to the selected elevator //
-////////////////////////////////////////////////////////////////
-
-function compute(levelAsk, directionAsk) {
-
-    var cage_1 = cage1.counterStep(levelAsk, directionAsk);
-    console.log("Step Cage1 :" + cage_1);
-    var cage_2 = cage2.counterStep(levelAsk, directionAsk);
-    console.log("Step Cage2 :" + cage_2);
-    battery_compute_BestTimingCage(cage_1, cage_2);
-
-    if (cage_1 < cage_2 && directionAsk === "up") {
-        if (cage1.levelActual < levelAsk) {
-            cage1.listUp.push(levelAsk);
-            console.log(cage1.listUp);
-            cage1.move();
-        } else if (cage1.levelActual > levelAsk) {
-            cage1.listDown.push(levelAsk);
-            console.log("lisDown cage1 : " + cage1.listDown);
-            cage1.move();
-        } else {
-            cage1.direction = "idle";
-            cage1.open_door();
-        }
-
-
-
-
-    } else if (cage_1 < cage_2 && directionAsk === "down") {
-        if (cage1.levelActual < levelAsk) {
-            cage1.listUp.push(levelAsk);
-            console.log(cage1.listUp);
-            cage1.move();
-        } else if (cage1.levelActual > levelAsk) {
-            cage1.listDown.push(levelAsk);
-            console.log("lisDown cage1 : " + cage1.listDown);
-            cage1.move();
-        } else {
-            cage1.direction = "idle";
-            cage1.open_door();
-        }
-        cage1.move();
-    } else if (cage_2 <= cage_1 && directionAsk === "up") {
-        if (cage2.levelActual < levelAsk) {
-            cage2.listUp.push(levelAsk);
-            console.log(cage2.listUp);
-            cage2.move();
-        } else if (cage1.levelActual > levelAsk) {
-            cage2.listDown.push(levelAsk);
-            console.log("lisDown cage1 : " + cage2.listDown);
-            cage2.move();
-        } else {
-            cage2.direction = "idle";
-            cage2.open_door();
-        }
-        cage2.move();
-
-
-    } else if (cage_2 <= cage_1 && directionAsk === "down") {
-        if (cage2.levelActual < levelAsk) {
-            cage2.listUp.push(levelAsk);
-            console.log(cage2.listUp);
-            cage2.move();
-        } else if (cage1.levelActual > levelAsk) {
-            cage2.listDown.push(levelAsk);
-            console.log("lisDown cage1 : " + cage2.listDown);
-            cage2.move();
-        } else {
-            cage2.direction = "idle";
-            cage2.open_door();
-        }
-
-    }
-
-
-};
-/////////////////////////////
-/////some Scenario //////////
-////////////////////////////////////////////////////////
-// constructor(id,column,battery,levelActual,direction) 
-// initialised cages //
-/////////////////////////////////////////    
-cage1 = new Cage("cage1", 1, 1, 8, "idle");
-cage2 = new Cage("cage2", 1, 1, 1, "idle");
-///////////////////////////////////////////
-
-//call inside the elevator
-//1er scenario : 
-
-// cage1.Call_level(5);
-// cage2.Call_level(2);
-
-// cage1.Call_level(3);
-// cage1.Call_level(7);
-// cage1.Call_level(4);
-// compute(3,'up'); 
-
-//call outside the elevator
-//compute(levelAsk, direction)
-compute(6, "down");
-
-compute(8, "up");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print("hello word")
+class Cage:
+    def __init__(self,id, levelActual, direction):
+        self.id = id
+        self.battery = 1         
+        self.column = 1
+        self.statusDoor = "closed"
+        self.position_door_x = 0 # close: 0 / open : 150
+        self.timing_door = 3000 
+        self.timing_floor = 20000
+        self.listUp = []        
+        self.listUpOrder = [] 
+        self.listDown = []   
+        self.listDownOrder = [] 
+        self.direction = direction 
+        self.levelActual = levelActual 
+        self.sensor_status_cage = "Clear"  
+        self.close_button = "inactif" 
+        self.open_button = "inactif"
+
+        
+    def display_status_cage(self):
+        print('status_cage: '+ str(self.id) + ' , level Actual: '+ str(self.levelActual) + ' , direction: '+ str(self.direction))
+    def call_level(self,levelCall):
+        print('level call: '+str(levelCall))
+        if self.levelActual > levelCall:
+            print("level actual is biggest then the level call so we down")
+        elif self.levelActual < levelCall:
+            print("level actual is lower then the level call so we up")
+        else:  print("level actual is the same then the call")
+    def insert_in_to_listUp():
+        pass
+    def insert_in_to_listDown():
+        pass
+
+
+# instance my object cag1 with this://def __init__(self,id, levelActual, direction)://
+cage1 = Cage(1, 1, "idle")
+cage2 = Cage(2, 5, "idle")
+
+# display  status cage:id
+cage1.display_status_cage()
+cage2.display_status_cage()
+
+# function call
+def call():
+    id = int(input('Enter cage number ? : '))
+    call_destination = int(input('Enter your destination ? : '))
+    if id == 1:
+        cage1.call_level(call_destination)
+    elif id == 2:
+        cage2.call_level(call_destination)
+    else: print('We have not this cage yet')
+    
+    
+
+
+call()
+# display call inside
+# cage_choice = input('Enter the number of cage you choice ? : ')
+# floor_destination_inside = int(input('Enter the floor destination ? : '))
+# cage1.call_level(floor_destination_inside)    
+  
